8a2565d7dcc4c4a6a8f628f2753f4a66dbc9231a
21E51A <akashpaiavula2003@gmail.com>
2026-01-28 20:11:14 +0530

    updated ingress

--- frontend/src/api/task.api.js
+++ frontend/src/api/task.api.js
@@ -1,11 +1,10 @@
-import api from "./axios";
-
+// ✅ CORRECT (no trailing slash)
 export const getTasks = async () => {
-  const res = await api.get("/tasks/");
+  const res = await api.get("/tasks");
   return res.data;
 };
 
 export const createTask = async (task) => {
-  const res = await api.post("/tasks/", task);
+  const res = await api.post("/tasks", task);
   return res.data;
 };
