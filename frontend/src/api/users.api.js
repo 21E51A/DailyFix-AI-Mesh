bcfda69a0714983f0fb97e38a9c1fb5aad739a21
21E51A <akashpaiavula2003@gmail.com>
2026-01-28 21:24:16 +0530

    updated frontend

--- frontend/src/api/users.api.js
+++ frontend/src/api/users.api.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export const registerUser = async (user) => {
-  const res = await api.post("/users", user); // ❌ NO trailing slash
+  const res = await api.post("/users", user); // 🔥 trailing slash REQUIRED
   return res.data;
 };
